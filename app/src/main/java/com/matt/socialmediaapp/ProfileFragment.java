a5087239df336749a1246420e162e587bb873326
Mateusz <mateuszd2411@gmail.com>
2020-05-01 20:44:14 +0200

    We have to get info of currently signed in user. We can get it using user's email or uid I'm gonna retrieve user detail using email By using orderByChild query we will show the detail from a node whose key named email has value equal to currently signed in email. It will search all nodes, where the key matches it will get its detail

--- app/src/main/java/com/matt/socialmediaapp/ProfileFragment.java
+++ app/src/main/java/com/matt/socialmediaapp/ProfileFragment.java
@@ -3,11 +3,24 @@ package com.matt.socialmediaapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 
 /**
@@ -15,6 +28,16 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
+
+    //firebase
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    //views
+    ImageView avatarIv;
+    TextView nameTv, emailTv, phoneTv;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -25,7 +48,64 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //init firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+
+        //init views
+        avatarIv = view.findViewById(R.id.avatarIv);
+        nameTv = view.findViewById(R.id.nameTv);
+        emailTv = view.findViewById(R.id.emailTv);
+        phoneTv = view.findViewById(R.id.phoneTv);
+
+        /*We have to get info of currently signed in user. We can get it using user's email or uid
+        I'm gonna retrieve user detail using email
+        By using orderByChild query we will show the detail from a node whose key named email has
+        value equal to currently signed in email.
+        It will search all nodes, where the key matches it will get its detail*/
+
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                //check until required data get
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    //get data from database
+                    String name = "" + ds.child("name").getValue();
+                    String email = "" + ds.child("email").getValue();
+                    String phone = "" + ds.child("phone").getValue();
+                    String image = "" + ds.child("image").getValue();
+
+                    //set data to fragment_profile.xml
+                    nameTv.setText(name);
+                    emailTv.setText(email);
+                    phoneTv.setText(phone);
+                    try {
+                        //if image is received then set
+                        Picasso.get().load(image).into(avatarIv);
+                    } catch (Exception e) {
+                        //if there is any exception while getting image then set default
+                        Picasso.get().load(R.drawable.ic_add_image).into(avatarIv);
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        return view;
+
+
     }
 
 }
