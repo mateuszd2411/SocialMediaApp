785ac7b5ce1b77ad9e3efb28fcad2e879131751d
Mateusz <mateuszd2411@gmail.com>
2020-05-01 10:15:36 +0200

    Added register and login button in Main Activity

--- app/src/main/java/com/matt/socialmediaapp/MainActivity.java
+++ app/src/main/java/com/matt/socialmediaapp/MainActivity.java
@@ -3,12 +3,30 @@ package com.matt.socialmediaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //views
+    Button mRegisterBtn, mLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init views
+        mRegisterBtn = findViewById(R.id.register_btn);
+        mLoginBtn = findViewById(R.id.login_btn);
+
+        //handle register burton click
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
