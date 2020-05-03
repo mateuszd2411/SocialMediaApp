2fd06146d24a96d76cf28e5e75435cab240e5258
Mateusz <mateuszd2411@gmail.com>
2020-05-03 11:43:36 +0200

    I added ModelChat(getters, setters, constructions). AdapterChar with extends RecyclerView.Adapter<AdapterChat.MyHolder>

---
+++ app/src/main/java/com/matt/socialmediaapp/adapters/AdapterChat.java
@@ -0,0 +1,65 @@
+package com.matt.socialmediaapp.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.matt.socialmediaapp.R;
+import com.matt.socialmediaapp.models.ModelChat;
+
+import java.util.List;
+
+public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
+
+    private static final int MSG_TYPE_LEFT = 0;
+    private static final int MSG_TYPE_RIGHT = 1;
+    Context context;
+    List<ModelChat> chatList;
+    String imageUrl;
+
+    public AdapterChat(Context context, List<ModelChat> chatList, String imageUrl) {
+        this.context = context;
+        this.chatList = chatList;
+        this.imageUrl = imageUrl;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    //view holder class
+    class MyHolder extends RecyclerView.ViewHolder {
+
+        //views
+        ImageView profielIv;
+        TextView messageTv, timeTv, isSeenTv;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //init views
+            profielIv = itemView.findViewById(R.id.profileIv);
+            messageTv = itemView.findViewById(R.id.messageTv);
+            timeTv = itemView.findViewById(R.id.timeTv);
+            isSeenTv = itemView.findViewById(R.id.isSeenTv);
+        }
+    }
+
+}
