0e8c1c2ab00d54983e40c73e8c868a8d6de1094e
Mateusz <mateuszd2411@gmail.com>
2020-05-23 14:49:20 +0200

    I added Authorization:key from Firebase project settings

---
+++ app/src/main/java/com/matt/socialmediaapp/notofications/APIService.java
@@ -0,0 +1,17 @@
+package com.matt.socialmediaapp.notofications;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @Headers({
+            "Content-Type:application/json",
+            "Authorization:key=AAAAD5HN2F4:APA91bHKuRD9_lUUPCpZ0GOk2aKTBeEBJ6F5PLpZjFpYcgDGNCQjnMMkWYzm2Zdf8Dk48Dj0rWz8LME_KJxrRds40hgRYDwAsQDmH0G1wQdz3kr_DGhqGOW5SkDYfPi2Hmxy7KQIrVtR"
+
+    })
+
+    @POST("fcm/send")
+    Call<Response> sendNotification(@Body Sender body);
+}
