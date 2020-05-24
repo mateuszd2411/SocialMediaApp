43b86066d99ea554ba4031d61ac649b3533bbd8b
Mateusz <mateuszd2411@gmail.com>
2020-05-24 10:18:57 +0200

    [Publish Post] In AddPostActivity added actionBar, checkUserStatus(), OptionsMenu. Added edit text style(gradient) and design for activity_add_post.xml

--- app/src/main/java/com/matt/socialmediaapp/AddPostActivity.java
+++ app/src/main/java/com/matt/socialmediaapp/AddPostActivity.java
@@ -1,14 +1,86 @@
 package com.matt.socialmediaapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AddPostActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
+
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Add New Post");
+        //enable back button in actionbar
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUserStatus();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkUserStatus();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkUserStatus();
+    }
+
+    private void checkUserStatus() {
+        //get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            //user is signed in stay here
+            //set email of logged in user
+        } else {
+            //user not signed in, go to MainActivity
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();    //go to previous activity
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        menu.findItem(R.id.action_add_post).setVisible(false);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //get item id
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
