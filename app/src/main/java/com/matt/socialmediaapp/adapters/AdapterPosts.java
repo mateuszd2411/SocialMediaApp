cf1d2a9306c9d8e759c274648418f1c79d8b3b5e
Mateusz <mateuszd2411@gmail.com>
2020-05-24 16:59:52 +0200

    [Show Posts] Added ModelPost and AdapterPosts with extends RecyclerView and init views (row_post.xml)

---
+++ app/src/main/java/com/matt/socialmediaapp/adapters/AdapterPosts.java
@@ -0,0 +1,72 @@
+package com.matt.socialmediaapp.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.matt.socialmediaapp.R;
+import com.matt.socialmediaapp.models.ModelPost;
+
+import java.util.List;
+
+public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder>{
+
+    Context context;
+    List<ModelPost> postList;
+
+    public AdapterPosts(Context context, List<ModelPost> postList) {
+        this.context = context;
+        this.postList = postList;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    //view holder class
+    class MyHolder extends RecyclerView.ViewHolder {
+
+        //views from row_post.xml
+        ImageView uPictureIv, pImageIv;
+        TextView uNameTv, pTimeTv, pTitleTv, pDescriptionTv, pLikesTv;
+        ImageButton moreBtn;
+        Button likeBtn, commentBtn, shareBtn;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //init views
+            uPictureIv = itemView.findViewById(R.id.uPictureIv);
+            pImageIv = itemView.findViewById(R.id.pImageIv);
+            uNameTv = itemView.findViewById(R.id.uNameTv);
+            pTimeTv = itemView.findViewById(R.id.pTimeTv);
+            pTitleTv = itemView.findViewById(R.id.pTitleTv);
+            pDescriptionTv = itemView.findViewById(R.id.pDescriptionTv);
+            pLikesTv = itemView.findViewById(R.id.pLikesTv);
+            moreBtn = itemView.findViewById(R.id.moreBtn);
+            likeBtn = itemView.findViewById(R.id.likeBtn);
+            commentBtn = itemView.findViewById(R.id.commentBtn);
+            shareBtn = itemView.findViewById(R.id.shareBtn);
+        }
+    }
+}
+
