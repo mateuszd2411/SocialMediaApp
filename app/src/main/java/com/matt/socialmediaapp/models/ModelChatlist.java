4de5ee24f6c249e6d6e1517fc142bdcef33d0c64
mateuszd2411 <mateuszd2411@gmail.com>
2020-05-27 20:12:56 +0200

    Added row_chatlist.xml design. onlineStatus, typingTo for ModelUser(getter, setter, constructors).In ChatActivity create chatList node/child in firebase database

---
+++ app/src/main/java/com/matt/socialmediaapp/models/ModelChatlist.java
@@ -0,0 +1,20 @@
+package com.matt.socialmediaapp.models;
+
+public class ModelChatlist {
+    String id;      //we will need this id to get chat list, sender/receiver uid
+
+    public ModelChatlist() {
+    }
+
+    public ModelChatlist(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
