12dadf21a7d3832b7a24cc50ff7c2edb402d605e
mat123123123 <123mateuszdz@gmail.com>
2020-05-27 09:32:47 +0200

    Added Actionbar amd initialized views for PostDetailActivity

--- app/src/main/java/com/matt/socialmediaapp/PostDetailActivity.java
+++ app/src/main/java/com/matt/socialmediaapp/PostDetailActivity.java
@@ -1,14 +1,61 @@
 package com.matt.socialmediaapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class PostDetailActivity extends AppCompatActivity {
+
+    //views
+    ImageView uPictureIv, pImageIv;
+    TextView nameTv, pTimeTv, pTitleTv, pDescriptionTv, pLikesTv;
+    ImageButton moreBtn;
+    Button likeBtn, shareBtn;
+    LinearLayout profileLayout;
+
+    //add cements views
+    EditText commentEt;
+    ImageButton sendBtn;
+    ImageView cAvatarIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_detail);
+
+        //Actionbar and its properties
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Post Detail");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        //init views
+        uPictureIv = findViewById(R.id.uPictureIv);
+        pImageIv = findViewById(R.id.pImageIv);
+        nameTv = findViewById(R.id.uNameTv);
+        pTimeTv = findViewById(R.id.pTimeTv);
+        pTitleTv = findViewById(R.id.pTitleTv);
+        pDescriptionTv = findViewById(R.id.pDescriptionTv);
+        pLikesTv = findViewById(R.id.pLikesTv);
+        moreBtn = findViewById(R.id.moreBtn);
+        likeBtn = findViewById(R.id.likeBtn);
+        shareBtn = findViewById(R.id.shareBtn);
+        profileLayout = findViewById(R.id.profileLayout);
+
+        commentEt = findViewById(R.id.commentEt);
+        sendBtn = findViewById(R.id.sendBtn);
+        cAvatarIv = findViewById(R.id.cAvatarIv);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
