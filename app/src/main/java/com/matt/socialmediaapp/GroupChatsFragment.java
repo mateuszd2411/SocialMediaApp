522f827a4c6726a9f519088530f9a885dce98f58
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-03 19:54:39 +0200

    Added recycler view to fragment_groupchatlist.xml and set to displaying chat groups list in fragment

--- app/src/main/java/com/matt/socialmediaapp/GroupChatsFragment.java
+++ app/src/main/java/com/matt/socialmediaapp/GroupChatsFragment.java
@@ -1,13 +1,35 @@
 package com.matt.socialmediaapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.matt.socialmediaapp.adapters.AdapterGroupChatList;
+import com.matt.socialmediaapp.models.ModelGroupChatList;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,6 +37,12 @@ import android.view.ViewGroup;
  */
 public class GroupChatsFragment extends Fragment {
 
+    private RecyclerView groupsRv;
+
+    private FirebaseAuth firebaseAuth;
+
+    private ArrayList<ModelGroupChatList> groupChatLists;
+    private AdapterGroupChatList adapterGroupChatList;
 
     public GroupChatsFragment() {
         // Required empty public constructor
@@ -25,7 +53,157 @@ public class GroupChatsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_groupchats, container, false);
+        View view = inflater.inflate(R.layout.fragment_groupchats, container, false);
+
+        groupsRv = view.findViewById(R.id.groupsRv);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loadGroupChatList();
+
+        return view;
+    }
+
+    private void loadGroupChatList() {
+        groupChatLists = new ArrayList<>();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Groups");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                groupChatLists.clear();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    //if current user's uid exists in Participants list of group then show that group
+                    if (ds.child("Participants").child(firebaseAuth.getUid()).exists()) {
+                        ModelGroupChatList model = ds.getValue(ModelGroupChatList.class);
+                        groupChatLists.add(model);
+                    }
+                }
+                adapterGroupChatList = new AdapterGroupChatList(getActivity(), groupChatLists);
+                groupsRv.setAdapter(adapterGroupChatList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void searchGroupChatList(final String query) {
+        groupChatLists = new ArrayList<>();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Groups");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                groupChatLists.clear();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    //if current user's uid exists in Participants list of group then show that group
+                    if (ds.child("Participants").child(firebaseAuth.getUid()).exists()) {
+
+                        //search by group title
+                        if (ds.child("groupTitle").toString().toLowerCase().contains(query.toLowerCase())) {
+                            ModelGroupChatList model = ds.getValue(ModelGroupChatList.class);
+                            groupChatLists.add(model);
+                        }
+
+                    }
+                }
+                adapterGroupChatList = new AdapterGroupChatList(getActivity(), groupChatLists);
+                groupsRv.setAdapter(adapterGroupChatList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);        //to show menu option in fragment
+        super.onCreate(savedInstanceState);
+    }
+
+
+    /*
+    inflate options menu
+     */
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        //inflating menu
+        inflater.inflate(R.menu.menu_main, menu);
+
+        //hide icons from this fragment
+        menu.findItem(R.id.action_add_post).setVisible(false);
+        menu.findItem(R.id.action_settings).setVisible(false);
+
+        //SearchView
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+
+        //search listener
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                //called when user press search button from keyboard
+                //if search query is not empty then search
+                if (!TextUtils.isEmpty(s.trim())) {
+                    //search text contains text, search it
+                    searchGroupChatList(s);
+                } else {
+                    //search text empty, get all users
+                    loadGroupChatList();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                //called whenever user press any single letter
+                //if search query is not empty then search
+                if (!TextUtils.isEmpty(s.trim())) {
+                    //search text contains text, search it
+                    searchGroupChatList(s);
+                } else {
+                    //search text empty, get all users
+                    loadGroupChatList();
+                }
+                return false;
+            }
+        });
+
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    //handle menu item click
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //get item id
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        else if (id == R.id.action_create_group) {
+            //go to GroupCreateActivity activity
+            startActivity(new Intent(getActivity(), GroupCreateActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void checkUserStatus() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user == null) {
+            //user not signed in, go to MainActivity
+            startActivity(new Intent(getActivity(), MainActivity.class));
+            getActivity().finish();
+        }
     }
 
 }
