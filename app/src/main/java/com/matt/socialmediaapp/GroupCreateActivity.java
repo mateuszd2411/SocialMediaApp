68d147810d67ea601bff1dda6ad96688a4c9fb41
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-03 13:53:17 +0200

    Added add picture for group chat avatar(with permissions for camera and gallery)

--- app/src/main/java/com/matt/socialmediaapp/GroupCreateActivity.java
+++ app/src/main/java/com/matt/socialmediaapp/GroupCreateActivity.java
@@ -1,20 +1,54 @@
 package com.matt.socialmediaapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class GroupCreateActivity extends AppCompatActivity {
+
+    //permission constants
+    private static final int CAMERA_REQUEST_CODE = 100;
+    private static final int STORAGE_REQUEST_CODE = 200;
+    //image pick constants
+    private static final int IMAGE_PICK_CAMERA_CODE = 300;
+    private static final int IMAGE_PICK_GALLERY_CODE = 400;
+    //permissions arrays
+    private String[] cameraPermissions;
+    private String[] storagePermissions;
+    //picked image uri
+    private Uri image_uri = null;
 
     //actionbar
     private ActionBar actionBar;
 
     //firebase auth
     private FirebaseAuth firebaseAuth;
+
+    //UI views
+    private ImageView groupIconIv;
+    private EditText groupTitleEt, groupDescriptionEt;
+    private FloatingActionButton createGroupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +60,103 @@ public class GroupCreateActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("Create Group");
 
+        //init Ui views
+        groupIconIv = findViewById(R.id.groupIconIv);
+        groupTitleEt = findViewById(R.id.groupTitleEt);
+        groupDescriptionEt = findViewById(R.id.groupDescriptionEt);
+        createGroupBtn = findViewById(R.id.createGroupBtn);
+
+        //init permissions arrays
+        cameraPermissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
         firebaseAuth = FirebaseAuth.getInstance();
         checkUser();
+
+        //pick image
+        groupIconIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showImagePickDialog();
+            }
+        });
+
+        //handle click event
+        createGroupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void showImagePickDialog() {
+        //options to pick image from
+        String[] options = {"Camera", "Gallery"};
+        //dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick Image: ")
+                .setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //handle clicks
+                        if (i == 0) {
+                            //camera clicked
+                            if (!checkCameraPermissions()) {
+                                requestCameraPermissions();
+                            } else {
+                                pickFromCamera();
+                            }
+                        } else {
+                            //gallery clicked
+                            if (!checkStoragePermissions()) {
+                                requestStoragePermissions();
+                            } else {
+                                pickFromGallery();
+                            }
+                        }
+                    }
+                }).show();
+    }
+
+    private void pickFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE);
+    }
+
+    private void pickFromCamera() {
+        ContentValues cv = new ContentValues();
+        cv.put(MediaStore.Images.Media.TITLE, "Group Image Icon Title");
+        cv.put(MediaStore.Images.Media.DESCRIPTION, "Group Image Icon Description");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, cv);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(intent, IMAGE_PICK_CAMERA_CODE);
+    }
+
+    private boolean checkStoragePermissions() {
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void requestStoragePermissions() {
+        ActivityCompat.requestPermissions(this, storagePermissions, STORAGE_REQUEST_CODE);
+    }
+
+    private boolean checkCameraPermissions() {
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+
+        return result && result1;
+    }
+
+    private void requestCameraPermissions() {
+        ActivityCompat.requestPermissions(this, cameraPermissions, CAMERA_REQUEST_CODE);
     }
 
     private void checkUser() {
@@ -41,5 +170,57 @@ public class GroupCreateActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //handle permission result
+        switch (requestCode) {
+            case CAMERA_REQUEST_CODE:{
+                if (grantResults.length > 0) {
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted && storageAccepted) {
+                        //permission allowed
+                        pickFromCamera();
+                    } else {
+                        //both or one is denied
+                        Toast.makeText(this, "Camera & Storage permissions are required", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+            case STORAGE_REQUEST_CODE:{
+                if (grantResults.length > 0) {
+                    boolean storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (storageAccepted) {
+                        //permissions allowed
+                        pickFromGallery();
+                    } else {
+                        Toast.makeText(this, "Storage permissions required", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //handle image pick result
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                //was picked from gallery
+                image_uri = data.getData();
+                //set to imageView
+                groupIconIv.setImageURI(image_uri);
+            } else if (requestCode == IMAGE_PICK_CAMERA_CODE) {
+                //was picked from camera
+                //set to imageView
+                groupIconIv.setImageURI(image_uri);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
