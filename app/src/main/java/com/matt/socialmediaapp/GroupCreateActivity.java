8561d7c7f13e6263a4c2fe0f587a50ab0028e170
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-03 12:55:14 +0200

    Added design for activity_group_create.xml, added actionBar and checkUser for GroupCreateActivity

--- app/src/main/java/com/matt/socialmediaapp/GroupCreateActivity.java
+++ app/src/main/java/com/matt/socialmediaapp/GroupCreateActivity.java
@@ -1,14 +1,45 @@
 package com.matt.socialmediaapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class GroupCreateActivity extends AppCompatActivity {
+
+    //actionbar
+    private ActionBar actionBar;
+
+    //firebase auth
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_create);
+
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("Create Group");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+    }
+
+    private void checkUser() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            actionBar.setTitle(firebaseAuth.getCurrentUser().getEmail());
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
