b936b4555f16b2d6fe33ff5b9a09d503fadc90c2
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-03 19:03:15 +0200

    Added Model and Adapter for Group chat

---
+++ app/src/main/java/com/matt/socialmediaapp/adapters/AdapterGroupChatList.java
@@ -0,0 +1,88 @@
+package com.matt.socialmediaapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.matt.socialmediaapp.R;
+import com.matt.socialmediaapp.models.ModelGroupChatList;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class AdapterGroupChatList extends RecyclerView.Adapter<AdapterGroupChatList.HolderGroupChatList>{
+
+    private Context context;
+    private ArrayList<ModelGroupChatList> groupChatLists;
+
+    public AdapterGroupChatList(Context context, ArrayList<ModelGroupChatList> groupChatLists) {
+        this.context = context;
+        this.groupChatLists = groupChatLists;
+    }
+
+    @NonNull
+    @Override
+    public HolderGroupChatList onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //inflate layout
+        View view = LayoutInflater.from(context).inflate(R.layout.row_groupchat_list, parent, false);
+
+        return new HolderGroupChatList(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HolderGroupChatList holder, int position) {
+
+        //get data
+        ModelGroupChatList model = groupChatLists.get(position);
+        String groupId = model.getGroupId();
+        String groupIcon = model.getGroupIcon();
+        String groupTitle = model.getGroupTitle();
+
+        //set data
+        holder.groupTitleTv.setText(groupTitle);
+        try {
+            Picasso.get().load(groupIcon).placeholder(R.drawable.ic_group_primary).into(holder.groupIconIv);
+        } catch (Exception e) {
+            holder.groupIconIv.setImageResource(R.drawable.ic_group_primary);
+        }
+
+        //handle group click
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return groupChatLists.size();
+    }
+
+    //view holder class
+    class HolderGroupChatList extends RecyclerView.ViewHolder {
+
+        //ui views
+        private ImageView groupIconIv;
+        private TextView groupTitleTv, nameTv, messageTv, timeTv;
+
+        public HolderGroupChatList(@NonNull View itemView) {
+            super(itemView);
+
+            groupIconIv = itemView.findViewById(R.id.groupIconIv);
+            groupTitleTv = itemView.findViewById(R.id.groupTitleTv);
+            nameTv = itemView.findViewById(R.id.nameTv);
+            messageTv = itemView.findViewById(R.id.messageTv);
+            timeTv = itemView.findViewById(R.id.timeTv);
+        }
+    }
+
+}
