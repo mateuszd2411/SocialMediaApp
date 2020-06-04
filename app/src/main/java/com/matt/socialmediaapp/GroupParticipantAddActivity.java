d2912a87ce2f73d2ede7ea00279187ac7ef74bae
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-04 14:39:36 +0200

    Added GroupParticipantAddActivity, row_participant_add.xml (+design), AdapterParticipantAdd (onCreateViewHolder, onBindViewHolder)

---
+++ app/src/main/java/com/matt/socialmediaapp/GroupParticipantAddActivity.java
@@ -0,0 +1,14 @@
+package com.matt.socialmediaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupParticipantAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_participant_add);
+    }
+}
---
+++ app/src/main/java/com/matt/socialmediaapp/adapters/AdapterParticipantAdd.java
@@ -0,0 +1,82 @@
+package com.matt.socialmediaapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.matt.socialmediaapp.R;
+import com.matt.socialmediaapp.models.ModelUser;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class AdapterParticipantAdd extends RecyclerView.Adapter<AdapterParticipantAdd.HolderParticipantAdd>{
+
+    private Context context;
+    private ArrayList<ModelUser> userList;
+    private String groupId, myGroupRole;    //creator/admin/participant
+
+    public AdapterParticipantAdd(Context context, ArrayList<ModelUser> userList, String groupId, String myGroupRole) {
+        this.context = context;
+        this.userList = userList;
+        this.groupId = groupId;
+        this.myGroupRole = myGroupRole;
+    }
+
+    @NonNull
+    @Override
+    public HolderParticipantAdd onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //inflate layout
+        View view = LayoutInflater.from(context).inflate(R.layout.row_participant_add, parent, false);
+
+        return new HolderParticipantAdd(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HolderParticipantAdd holder, int position) {
+        //get data
+        ModelUser modelUser = userList.get(position);
+        String name = modelUser.getName();
+        String email = modelUser.getEmail();
+        String image = modelUser.getImage();
+        String uid = modelUser.getUid();
+
+        //set data
+        holder.nameTv.setText(name);
+        holder.emailTv.setText(email);
+        try {
+            Picasso.get().load(image).placeholder(R.drawable.ic_default_img).into(holder.avatarIv);
+        } catch (Exception e) {
+            holder.avatarIv.setImageResource(R.drawable.ic_default_img);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    class HolderParticipantAdd extends RecyclerView.ViewHolder{
+
+        private ImageView avatarIv;
+        private TextView nameTv, emailTv, statusTv;
+
+
+        public HolderParticipantAdd(@NonNull View itemView) {
+            super(itemView);
+
+            avatarIv = itemView.findViewById(R.id.avatarIv);
+            nameTv = itemView.findViewById(R.id.nameTv);
+            emailTv = itemView.findViewById(R.id.emailTv);
+            statusTv = itemView.findViewById(R.id.statusTv);
+        }
+    }
+
+
+}
