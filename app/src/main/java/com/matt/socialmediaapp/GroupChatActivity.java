749a10edeb0051a788a6e344a3d159a1d5bf289d
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-04 11:13:14 +0200

    In GroupChatActivity added views etc. sendMessage, loadGroupInfo

--- app/src/main/java/com/matt/socialmediaapp/GroupChatActivity.java
+++ app/src/main/java/com/matt/socialmediaapp/GroupChatActivity.java
@@ -1,21 +1,135 @@
 package com.matt.socialmediaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 
 public class GroupChatActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
+
     private String groupId;
+
+    private Toolbar toolbar;
+    private ImageView groupIconIv;
+    private ImageButton attachBtn, sendBtn;
+    private TextView groupTitleTv;
+    private EditText messageEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_chat);
 
+        //init views
+        toolbar = findViewById(R.id.toolbar);
+        groupIconIv = findViewById(R.id.groupIconIv);
+        attachBtn = findViewById(R.id.attachBtn);
+        sendBtn = findViewById(R.id.sendBtn);
+        groupTitleTv = findViewById(R.id.groupTitleTv);
+        messageEt = findViewById(R.id.messageEt);
+
         //get id of the group
         Intent intent = getIntent();
         groupId = intent.getStringExtra("groupId");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        loadGroupInfo();
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //input data
+                String message = messageEt.getText().toString().trim();
+                //validate
+                if (TextUtils.isEmpty(message)) {
+                    Toast.makeText(GroupChatActivity.this, "Can'tsend emty message...", Toast.LENGTH_SHORT).show();
+                } else {
+                    //send message
+                    sendMessage(message);
+                }
+            }
+        });
+
+    }
+
+    private void sendMessage(String message) {
+        //timestamp
+        String timestamp = "" + System.currentTimeMillis();
+
+        //setup message data
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", "" + firebaseAuth.getUid());
+        hashMap.put("message", "" + message);
+        hashMap.put("timestamp", "" + timestamp);
+        hashMap.put("type", "" + "text");   //text/image/file
+
+        //add in db
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Groups");
+        ref.child(groupId).child("Messages").child(timestamp)
+                .setValue(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //message sent
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(GroupChatActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void loadGroupInfo() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Groups");
+        ref.orderByChild("groupId").equalTo(groupId)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                            String groupTitle = "" + ds.child("groupTitle").getValue();
+                            String groupDescription = "" + ds.child("groupDescription").getValue();
+                            String groupIcon = "" + ds.child("groupIcon").getValue();
+                            String timestamp = "" + ds.child("timestamp").getValue();
+                            String createdBy = "" + ds.child("createdBy").getValue();
+
+                            groupTitleTv.setText(groupTitle);
+                            try {
+                                Picasso.get().load(groupIcon).placeholder(R.drawable.ic_group_white).into(groupIconIv);
+                            } catch (Exception e) {
+                                groupIconIv.setImageResource(R.drawable.ic_group_white);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 }
