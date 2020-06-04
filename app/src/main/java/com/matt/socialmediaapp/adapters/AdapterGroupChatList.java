f10361ecf73614bccde16139a58250c01bd7a185
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-04 14:22:14 +0200

    Added displaying last message in group chat list(recycler view)

--- app/src/main/java/com/matt/socialmediaapp/adapters/AdapterGroupChatList.java
+++ app/src/main/java/com/matt/socialmediaapp/adapters/AdapterGroupChatList.java
@@ -2,6 +2,7 @@ package com.matt.socialmediaapp.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,19 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.matt.socialmediaapp.GroupChatActivity;
 import com.matt.socialmediaapp.R;
 import com.matt.socialmediaapp.models.ModelGroupChatList;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AdapterGroupChatList extends RecyclerView.Adapter<AdapterGroupChatList.HolderGroupChatList>{
 
@@ -46,6 +54,13 @@ public class AdapterGroupChatList extends RecyclerView.Adapter<AdapterGroupChatL
         String groupIcon = model.getGroupIcon();
         String groupTitle = model.getGroupTitle();
 
+        holder.nameTv.setText("");
+        holder.timeTv.setText("");
+        holder.messageTv.setText("");
+
+        //load last message and message time
+        loadLastMessage(model, holder);
+
         //set data
         holder.groupTitleTv.setText(groupTitle);
         try {
@@ -65,6 +80,56 @@ public class AdapterGroupChatList extends RecyclerView.Adapter<AdapterGroupChatL
             }
         });
 
+    }
+
+    private void loadLastMessage(ModelGroupChatList model, final HolderGroupChatList holder) {
+        //get last message from group
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Groups");
+        ref.child(model.getGroupId()).child("Messages").limitToLast(1)  //get last item(message) from that child
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                            //get data
+                            String message = "" + ds.child("message").getValue();
+                            String timestamp = "" + ds.child("timestamp").getValue();
+                            String sender = "" + ds.child("sender").getValue();
+
+                            //convert time stamp to dd/mm/yyyy hh:mm am/pm
+                            Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+                            cal.setTimeInMillis(Long.parseLong(timestamp));
+                            String dateTime = DateFormat.format("dd/MM/yyyy hh:mm aa", cal).toString();
+
+                            holder.messageTv.setText(message);
+                            holder.timeTv.setText(dateTime);
+
+                            //get info of sender of last message
+                            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+                            ref.orderByChild("uid").equalTo(sender)
+                                    .addValueEventListener(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                                String name = "" + ds.child("name").getValue();
+                                                holder.nameTv.setText(name);
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    });
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     @Override
