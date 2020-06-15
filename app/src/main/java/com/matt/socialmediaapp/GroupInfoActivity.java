3ff06f5c694f0d2e14344f2cc85cb85575346b05
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-15 17:06:01 +0200

    Added design for activity_group_info.xml

--- app/src/main/java/com/matt/socialmediaapp/GroupInfoActivity.java
+++ app/src/main/java/com/matt/socialmediaapp/GroupInfoActivity.java
@@ -1,14 +1,31 @@
 package com.matt.socialmediaapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class GroupInfoActivity extends AppCompatActivity {
 
+    private String groupId;
+
+    private ActionBar actionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_info);
+
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        groupId = getIntent().getStringExtra("groupId");
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
