de2d405fcad01aeaeae40d86302214a38828c729
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-16 14:24:05 +0200

    Added ui views in GroupInfoActivity. loadGroupInfo(), loadCreatorInfo(), loadGroupRole(), loadParticipants() from firebase database. Set addParticipantTv clickable to add new Participants to chat group

--- app/src/main/java/com/matt/socialmediaapp/GroupInfoActivity.java
+++ app/src/main/java/com/matt/socialmediaapp/GroupInfoActivity.java
@@ -1,15 +1,47 @@
 package com.matt.socialmediaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.matt.socialmediaapp.adapters.AdapterParticipantAdd;
+import com.matt.socialmediaapp.models.ModelUser;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class GroupInfoActivity extends AppCompatActivity {
 
     private String groupId;
+    private String myGroupRole;
+
+    private FirebaseAuth firebaseAuth;
 
     private ActionBar actionBar;
+
+    //ui views
+    private ImageView groupIconIv;
+    private TextView descriptionTv, createdByTv, editGroupTv, addParticipantTv, participantsTv, leaveGroupTv;
+    private RecyclerView participantsRv;
+
+    private ArrayList<ModelUser> userList;
+    private AdapterParticipantAdd adapterParticipantAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +52,166 @@ public class GroupInfoActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
 
+        groupIconIv = findViewById(R.id.groupIconIv);
+        descriptionTv = findViewById(R.id.descriptionTv);
+        createdByTv = findViewById(R.id.createdByTv);
+        editGroupTv = findViewById(R.id.editGroupTv);
+        addParticipantTv = findViewById(R.id.addParticipantTv);
+        leaveGroupTv = findViewById(R.id.leaveGroupTv);
+        participantsTv = findViewById(R.id.participantsTv);
+        participantsRv = findViewById(R.id.participantsRv);
+
         groupId = getIntent().getStringExtra("groupId");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        loadGroupInfo();
+        loadGroupRole();
+
+        addParticipantTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GroupInfoActivity.this, GroupParticipantAddActivity.class);
+                intent.putExtra("groupId", groupId);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void loadGroupInfo() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Groups");
+        ref.orderByChild("groupId").equalTo(groupId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    //get group info
+                    String groupId = "" + ds.child("groupId").getValue();
+                    String groupTitle = "" + ds.child("groupTitle").getValue();
+                    String groupDescription = "" + ds.child("groupDescription").getValue();
+                    String groupIcon = "" + ds.child("groupIcon").getValue();
+                    String createdBy = "" + ds.child("createdBy").getValue();
+                    String timestamp = "" + ds.child("timestamp").getValue();
+
+                    Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+                    cal.setTimeInMillis(Long.parseLong(timestamp));
+                    String dateTime = DateFormat.format("dd/MM/yyyy hh:mm aa", cal).toString();
+
+                    loadCreatorInfo(dateTime, createdBy);
+
+                    //set group inf
+                    actionBar.setTitle(groupTitle);
+                    descriptionTv.setText(groupDescription);
+
+                    try {
+                        Picasso.get().load(groupIcon).placeholder(R.drawable.ic_group_primary).into(groupIconIv);
+                    } catch (Exception e) {
+                        groupIconIv.setImageResource(R.drawable.ic_group_primary);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void loadCreatorInfo(final String dateTime, final String createdBy) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.orderByChild("uid").equalTo(createdBy).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String name = "" + ds.child("name").getValue();
+                    createdByTv.setText("Created by " + name + " on " + dateTime);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void loadGroupRole() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Groups");
+        ref.child(groupId).child("Participants").orderByChild("uid")
+                .equalTo(firebaseAuth.getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                            myGroupRole = "" + ds.child("role").getValue();
+                            actionBar.setSubtitle(firebaseAuth.getCurrentUser().getEmail() + " (" + myGroupRole);
+
+                            if (myGroupRole.equals("participant")) {
+                                editGroupTv.setVisibility(View.GONE);
+                                addParticipantTv.setVisibility(View.GONE);
+                                leaveGroupTv.setText("Leave Group");
+                            }
+                            else if (myGroupRole.equals("admin")) {
+                                editGroupTv.setVisibility(View.GONE);
+                                addParticipantTv.setVisibility(View.VISIBLE);
+                                leaveGroupTv.setText("Leave Group");
+                            }
+                            else if (myGroupRole.equals("creator")) {
+                                editGroupTv.setVisibility(View.VISIBLE);
+                                addParticipantTv.setVisibility(View.VISIBLE);
+                                leaveGroupTv.setText("Delete Group");
+                            }
+                        }
+                    }
+
+                    private void loadParticipants() {
+                        userList = new ArrayList<>();
+
+                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Groups");
+                        ref.child(groupId).child("Participants").addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                userList.clear();
+                                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                    //get uid from Group > Participants
+                                    String uid = "" + ds.child("uid").getValue();
+
+                                    //get info of user using uid we got above
+                                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+                                    ref.orderByChild("uid").equalTo(uid).addValueEventListener(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                                ModelUser modelUser = ds.getValue(ModelUser.class);
+
+                                                userList.clear();
+                                            }
+                                            //adapter
+                                            adapterParticipantAdd = new AdapterParticipantAdd(GroupInfoActivity.this, userList, groupId, myGroupRole);
+                                            //set adapter
+                                            participantsRv.setAdapter(adapterParticipantAdd);
+                                            participantsTv.setText("Participants (" + userList.size() + ")");
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    });
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     @Override
