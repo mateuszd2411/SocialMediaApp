c19f3c036c21035ba1cbd6fe6ffd4fed505db53c
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-26 11:34:29 +0200

    Start facebook login integration

--- app/src/main/java/com/matt/socialmediaapp/MainActivity.java
+++ app/src/main/java/com/matt/socialmediaapp/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class MainActivity extends AppCompatActivity {
 
     //views
